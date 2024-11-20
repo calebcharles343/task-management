aeb70c0d777c0520a76d5e337d3f7b03e18ce03d
Charles Caleb <charlesofficial9@gmail.com>
2024-11-20 04:51:49 +0100

    update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const errorHandler = require("./middlewares/errorHandler");
 const projectRouter = require("./routes/projectRouter");
 const taskRouter = require("./routes/taskRouter");
@@ -7,8 +8,9 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors());
 app.use("/api/projects", projectRouter);
-app.use('/api/projects', taskRouter);
+app.use("/api/projects", taskRouter);
 
 app.use(errorHandler);
 
--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router({ mergeParams: true });
 const {
-    getAllTasks,
-    createTask,
-    updateTask,
-    deleteTask
-} = require('../controllers/taskController');
+  getAllTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/taskController");
 
-router.get('/:project_id/tasks', getAllTasks);
-router.post('/:project_id/tasks', createTask);
-router.put('/:project_id/tasks/:task_id', updateTask);
-router.delete('/:project_id/tasks/:task_id', deleteTask)
+router.get("/:project_id/tasks/:id", getAllTasks);
+router.post("/:project_id/tasks", createTask);
+router.put("/:project_id/tasks/:task_id", updateTask);
+router.delete("/:project_id/tasks/:task_id", deleteTask);
 
 module.exports = router;
